637c81bba5d8070eba6d46af722a3c854947c859
João Porto <44210238+zetsbr@users.noreply.github.com>
2025-12-29 18:32:48 -0400

    ADD: Adicionando rota para obter o mapa do backend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from app.routes import world, note, token
+from app.repository.json_repository import JsonRepository
 
 app = FastAPI(
     title="Hexgrid RPG Backend",
@@ -14,3 +17,18 @@ app.include_router(token.router)
 @app.get("/")
 def root():
     return {"message": "Hex Map Backend is running"}
+
+@app.get("/world/map")
+def get_world_map():
+    world_repository = JsonRepository()
+    world = world_repository.load()
+
+    map_path = Path(world.map_image)
+
+    if not map_path.exists():
+        return {"status": "error", "message": "Failed to get world map image"}
+
+    return FileResponse(
+        path=map_path,
+        media_type="image/png"
+    )
