0e0bca13c37c36902b57ca3b987842271b7fd217
João Porto <44210238+zetsbr@users.noreply.github.com>
2025-12-29 19:11:39 -0400

    UPDATE: Ajustando local onde a rota de obter o mapa de background está.

--- backend/app/routes/world.py
+++ backend/app/routes/world.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from app.services.world import WorldService
 from app.models.world import World
+from app.repository.json_repository import JsonRepository
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 router = APIRouter(prefix="/world", tags=["world"])
 world_service = WorldService()
@@ -17,3 +20,18 @@ def get_world():
 def save_world(world: World):
     world_service.save_world(world)
     return {"status": "ok"}
+
+@router.get("/map")
+def get_world_map():
+    world_repository = JsonRepository()
+    world = world_repository.load()
+
+    map_path = Path(world.map_image)
+
+    if not map_path.exists():
+        return {"status": "error", "message": "Failed to get world map image"}
+
+    return FileResponse(
+        path=map_path,
+        media_type="image/png"
+    )
